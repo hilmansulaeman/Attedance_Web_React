5724510e5819da14fe1470d1be84833c74e872f3
Hilman Sulaeman <hilmansulaeman0504@gmail.com>
2023-09-26 16:02:26 +0700

    Halaman Dashboard Attendance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
 import './App.css';
+// import { Sidebar } from "./component/Sidebar";
+import Sidebar from './component/Sidebar';
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div className='flex'>
+      <div className='basis-[15%] h-[120vh]'>
+        <Sidebar />
+      </div>
+      <div className='basis-[85%] border'>d</div>
+    </div>
   )
 }
 
